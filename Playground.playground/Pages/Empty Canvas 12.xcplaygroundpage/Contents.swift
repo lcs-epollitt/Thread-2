c0c53e0b4914c77a6a5154a647ccc5c866ede3eb
Ella <epollitt@lcs.on.ca>
2022-01-27 10:05:30 -0500

    i hope this is the talking heads poster i can't tell lol. if it is, I finished like half of it - i just have the writing to do

--- Playground.playground/Pages/Empty Canvas 12.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 12.xcplaygroundpage/Contents.swift
@@ -4,7 +4,7 @@
  
  Set the size of your desired canvas by adjusting the constants on lines 7 and 8.
  */
-let preferredWidth = 600
+let preferredWidth = 400
 let preferredHeight = 600
 /*:
  ## Required code
@@ -29,6 +29,9 @@ let p = Pen(drawingUpon: canvas)
 // Show the canvas in the playground's live view
 PlaygroundPage.current.liveView = canvas
 
+
+// conditionally change the fill colour
+
 /*:
  ## Optional code
  
@@ -41,11 +44,9 @@ PlaygroundPage.current.liveView = canvas
  */
 
 // Move the origin from the bottom-left corner of the canvas to it's centre point
-canvas.translate(to: Point(x: canvas.width / 2,
-                           y: canvas.height / 2))
 
 // Show a grid
-canvas.drawAxes(withScale: true, by: 20, color: .black)
+canvas.drawAxes(withScale: true, by: 50, color: .black)
 
 /*:
  ## Add your code
@@ -56,22 +57,38 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
 
  */
 
-// Begin writing your code below (you can remove the examples shown)
 
-// Draw a circle, using the canvas object directly
-canvas.drawEllipse(at: Point(x: 100, y: 100), width: 25, height: 25)
+// Draw a grid of circles
 
-// Draw a vertical line, up and to the left
-p.drawTo(dx: -25, dy: 50)
 
-// Go back to origin
-p.goToOrigin()
 
-// Change the pen color
-p.penColor = .red
+    
+canvas.textColor = .red
+for xPosition in stride(from: 0, through: 400, by: 40) {
+    for yPosition in stride(from: 0, through: 400, by: 40) {
+        
+        if xPosition + yPosition <= 400
+            && xPosition > 0
+        && yPosition > 0 {
+            
+            canvas.fillColor = .purple
+        } else {
+            canvas.fillColor = .yellow
+        }
+        
+        // Draw the circle
+        canvas.drawEllipse(at: Point(x: xPosition,
+                                     y: yPosition),
+                           width: 36, height: 36)
+        
+        // Where is this circle?
+        canvas.drawText(message: "(\(xPosition), \n\(yPosition))",
+                        at: Point(x: xPosition, y: yPosition - 10),
+                        size: 8)
 
-// Draw a curve, down and to the right
-p.addArc(radius: 50, angle: -45)
+        
+    }
+}
 
 /*:
  ## Show the Live View
