b952dd53281dbb3864d80881b3b13c83219f7afb
Ella <epollitt@lcs.on.ca>
2022-06-09 14:24:46 -0400

    first x done

--- Playground.playground/Pages/Blank canvas to copy.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Blank canvas to copy.xcplaygroundpage/Contents.swift
@@ -16,12 +16,13 @@ let preferredHeight = 600
 import Cocoa
 import PlaygroundSupport
 import CanvasGraphics
+import Darwin
 
 // Create canvas
 let canvas = Canvas(width: preferredWidth, height: preferredHeight)
 
 // Create a turtle that can draw upon the canvas
-let turtle = Tortoise(drawingUpon: canvas)
+let t = Tortoise(drawingUpon: canvas)
 
 // Create a pen that can draw upon the canvas
 let p = Pen(drawingUpon: canvas)
@@ -40,9 +41,12 @@ PlaygroundPage.current.liveView = canvas
  
  */
 
+// Move the origin from the bottom-left corner of the canvas to it's centre point
+canvas.translate(to: Point(x: 100,
+                           y: 100))
+
 // Show a grid
 canvas.drawAxes(withScale: true, by: 20, color: .black)
-
 /*:
  ## Add your code
  
@@ -51,21 +55,112 @@ canvas.drawAxes(withScale: true, by: 20, color: .black)
  [Documentation](http://russellgordon.ca/CanvasGraphics/Documentation/) is available.
 
  */
-canvas.drawCurve(from: Point (x: 25, y: 25), to: Point (x: 80, y: 80), control1: Point (x: 100, y: 100), control2: Point (x: 200, y: 200))
+let scale = 20
+let diagonal = Int (sqrt(2)*Double(scale))
+
+canvas.highPerformance = false
+t.drawSelf()
+
+// prep for first angle
+t.penUp()
+t.forward(steps: 2 * scale)
+t.penDown()
+t.left(by: 135)
+
+// draw first line
+
+t.forward(steps: 2 * diagonal)
+
+// prep for second angle
+
+t.right(by: 90)
+
+// second line
+
+t.forward(steps: 2 * diagonal)
+
+// prep for 3rd qngle
+
+t.left(by: 90)
+
+//3rd line
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 4th
+
+t.right(by: 90)
+
+// 4th line
+
+t.forward(steps: 2 * diagonal)
 
 
-/*:
- ## Show the Live View
- Don't see any results?
- 
- Remember to show the Live View (1 then 2):
- 
- ![timeline](timeline.png "Timeline")
+// prep for 5th
 
- ## Use source control
- To keep your work organized, receive feedback, and earn a high grade in this course, regular use of source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source-control.png "Source Control")
- */
+t.right(by: 90)
+
+// 5th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 6th
+
+t.left(by: 90)
+
+// 6th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 7th
+
+t.right(by: 90)
+
+// 7th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 8th
+
+t.right(by: 90)
+
+// 8th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 9th
+
+t.left(by: 90)
+
+// 9th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 10th
+
+t.right(by: 90)
+
+// 10th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 11th
+
+t.right(by: 90)
+
+// 11th
+
+t.forward(steps: 2 * diagonal)
+
+// prep 4 12th
+
+t.left(by: 90)
+
+// 12th
+
+t.forward(steps: 2 * diagonal)
+
+
+
+
+
